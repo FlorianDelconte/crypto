e2f327d10110c1ebf87d9164941842bdf2499502
Florian <florian.delconte@protonmail.com>
2018-03-02 17:25:08 +0100

    ca marche fils de pute

--- ElGamal.py
+++ ElGamal.py
@@ -1,32 +1,42 @@
 import math
 import gmpy2
 import euclide
+import sys
 from random import randint
 from gmpy2 import mpz, digits
 
 g=2
 p=int("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE65381FFFFFFFFFFFFFFFF",16)
+p=gmpy2.mpz(p)
+'''
 cpt=0
 
 #######################################"TEST EUCLIDE"
-#for i in range(1,10001):
-#    a=gmpy2.mpz_rrandomb(gmpy2.random_state(randint(0,999999999999999)),1024)
-#    u, v=euclide.euclide(a,p)
-#    if (a*u+p*v==1):
-#        cpt=cpt+1
-#print (cpt)
+for i in range(1,10001):
+    a=gmpy2.mpz_rrandomb(gmpy2.random_state(randint(0,999999999999999)),1024)
+    u, v=euclide.euclide(a,p)
+    if (a*u+p*v==1):
+        cpt=cpt+1
+print (cpt)
+'''
 
 
 #######################################"TEST EXPMOD"
+
+'''
 for i in range(1,10001):
-    g=gmpy2.mpz_rrandomb(gmpy2.random_state(randint(0,999999999999999)),1024)
-    r=euclide.expMod(17,g,i)
-    if ((g**i)%17==r):
+    #g=gmpy2.mpz_rrandomb(gmpy2.random_state(randint(0,999999999999999)),1024)
+
+
+    r=euclide.expMod(p,g,i)
+    if ((g**i)%p==r):
         cpt=cpt+1
 print (cpt)
-
-
-
+'''
+a=88766687893365354535363737363536536327748498737938276467
+r=euclide.expMod(p,g,a)
+if(r==gmpy2.powmod(g,a,p)):
+    print("CA MARCHE FDPx")
 
 
 
--- euclide.py
+++ euclide.py
@@ -1,3 +1,6 @@
+import gmpy2
+from gmpy2 import mpz, digits
+
 def euclide(r0, r1):
     u0 = 1
     u1 = 0
@@ -21,22 +24,91 @@ def euclide(r0, r1):
      #print("u = %d, v = %d" %(u, v))
     return (u, v)
 
-
+#g=2
+#p=grand nombre premier
+#a= puissance de g
 def expMod(p, g, a):
+    a=gmpy2.mpz(a)
+    p=gmpy2.mpz(p)
+    g=gmpy2.mpz(g)
+
     if (a==1):
-        return g % p
-    elif (a%2==0):
-        return expMod(p, (g**2) % p, (a/2) % p) % p
-    elif ((a%2==1) and (a > 2)):
-        return (g * expMod(p, (g**2) % p, ((a-1)/2) % p)) % p
+        return gmpy2.f_mod(g , p)
+    elif (gmpy2.f_mod(a,2)==0):
+        #print("pair")
+        na=gmpy2.f_mod(gmpy2.div(a,2),p)
+        ng=gmpy2.f_mod(gmpy2.mul(g,g), p)
+        #return gmpy2.c_mod((expMod(p, ng, na) , p)
+        return gmpy2.f_mod(expMod(p, ng, na),p)
+    elif ((gmpy2.f_mod(a,2)==1) and (a>2)):
+        #and (a > 2)
+        #print("impair")
+        na=gmpy2.f_mod(gmpy2.div((a-1),2),p)
+        ng=gmpy2.f_mod(gmpy2.mul(g,g), p)
+        return  gmpy2.f_mod(gmpy2.mul(expMod(p, ng, na),g),p)
+        #return (g * expMod(p, (g**2) % p, ((a-1)/2) % p)) % p
 
 
 #def keyGen(p, g):
 
+'''void expMod(mpz_t res,mpz_t p,mpz_t g,mpz_t a) {
+//         On applique le modulo après chaque calcul
+    mpz_t mod2,gg,aa,un,deux;
+//     initialisation des variables
+    mpz_init(gg);
+    mpz_init(aa);
+    mpz_init(deux);
+    mpz_init(un);
+    mpz_init(mod2);
+    mpz_set_d(mod2,2);
+    mpz_set_d(deux,2);
+    mpz_set_d(un,1);
+//     Si a = 1 le résultat est g
+    if (mpz_cmp_d(a,1)==0) {
+        mpz_mod(gg,g,p);
+        mpz_set(res,gg);
+    }
+    else {
+//     Si a est pair le résultat est expMod(res,p,g²,a/2)
+        mpz_mod(mod2,a,mod2);
+        if (mpz_cmp_d(mod2,0)==0) {
+//            g²
+            mpz_mul(gg,g,g);
+//         a/2
+            mpz_fdiv_q(aa,a,deux);
+//         Appel récursif
+            expMod(res,p,gg,aa);
+            mpz_mod(res,res,p);
+        }
+//     Si a > 2 est impair le résultat est g*expMod(res,p,g²,(a-1)/2)
+        else {
+            if (mpz_cmp_d(a,2)>0) {
+//         g²
+                mpz_mul(gg,g,g);
+//         a-1
+                mpz_sub(aa,a,un);
+//         (a-1)/2
+                mpz_fdiv_q(aa,aa,deux);
+//         Appel récursif
+                expMod(res,p,gg,aa);
+                mpz_mod(res,res,p);
+//         g*expMod(...)
+                mpz_mul(res,g,res);
+                mpz_mod(res,res,p);
+            }
+        }
 
+    }
+//     Libère la mémoire
+    mpz_clear(mod2);
+    mpz_clear(gg);
+    mpz_clear(aa);
+    mpz_clear(deux);
+    mpz_clear(un);
+}
 
 
 u, v = euclide(325, 145)
 print("u =", u, "v =", v)
 
-print(expMod(17, 9, 1))
+print(expMod(17, 9, 1))'''
